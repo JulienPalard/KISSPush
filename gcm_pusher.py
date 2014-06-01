f1909fe7c72d559e835be3bf3d9870591f01ec5a
Julien Palard <julien@palard.fr>
2014-06-01 20:00:25 +0200

    Honor any Retry-After header.

--- gcm_pusher.py
+++ gcm_pusher.py
@@ -12,6 +12,10 @@ from gcm import messages_to_send, setup_logging, MySQL_schema_update
 class GCM_Pusher():
     def __init__(self):
         self.backoff = 0
+        self.log = logging.getLogger('kisspush')
+        self.headers = {'Content-Type': 'application/json',
+                        'Authorization': 'key=' + config['api_key']}
+        self.url = 'https://android.googleapis.com/gcm/send'
 
     def run(self):
         while True:
@@ -21,7 +25,7 @@ class GCM_Pusher():
                     sleep(self.backoff)
                 sleep(.5)
             except Exception:
-                logging.getLogger('kisspush').exception(
+                self.log.exception(
                     "Unhandled exception while pushing messages to GCM")
 
     def push_all(self):
@@ -33,12 +37,22 @@ class GCM_Pusher():
                 logging.getLogger('kisspush').exception(
                     "Unhandled exception while pushing a message to GCM")
 
+    def exponential_backoff(self, response):
+        retry_after = response.headers.get('Retry-After', None)
+        if retry_after is not None:
+            self.backoff = int(retry_after)
+        elif response == 200:
+            self.backoff = 0
+        elif response.status_code >= 500:
+            self.backoff = 1 if self.backoff == 0 else self.backoff * 2
+            self.log.info("Will backoff %d seconds after receiving a %d error",
+                          self.backoff, response.status_code)
+
     def push_one(self, message):
         """
         TODO Group by id_message, do not send them 1 by 1, google support
         batch send of the same message.
         """
-        log = logging.getLogger('kisspush')
         data = {'registration_ids': [message['registration_id']],
                 'data': {'msg': message['message']}}
         if message['collapse_key'] is not None:
@@ -46,22 +60,14 @@ class GCM_Pusher():
         if message['delay_while_idle']:
             message['delay_while_idle'] = True
         data = json.dumps(data)
-        headers = {'Content-Type': 'application/json',
-                   'Authorization': 'key=' + config['api_key']}
-        url = 'https://android.googleapis.com/gcm/send'
-        log.debug("Will send %s", data)
+        self.log.debug("Will send %s", data)
         try:
-            response = requests.post(url, data=data, headers=headers)
-            if response == 200:
-                self.backoff = 0
-            if response.status_code >= 500:
-                self.backoff = 1 if self.backoff == 0 else self.backoff * 2
-                log.info("Will backoff %d seconds after receiving a %d error",
-                         self.backoff, response.status_code)
+            response = requests.post(self.url, data=data, headers=self.headers)
+            self.exponential_backoff(response)
         except Exception:
-            log.exception("While sending a message to GCM")
+            self.log.exception("While sending a message to GCM")
         else:
-            log.info(response.content)
+            self.log.info(response.content)
 
 
 def parse_args(print_help=False):
